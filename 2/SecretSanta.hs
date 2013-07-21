298eda22a4d2195f4cdb89c3a4cfc3f2e6ff5b3b
Fulvio satta <yotavga@users.sourceforge.net>
2013-07-21 22:46:33 +0200

    Copyright note added.

--- 2/SecretSanta.hs
+++ 2/SecretSanta.hs
@@ -11,6 +11,8 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright © 2013 Fulvio Satta
 -}
 
 import Data.Char
